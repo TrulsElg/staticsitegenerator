cdb3e5988f6a2ab0d4d6372c8d84fa93346254ec
TrulsElg <trulseb@gmail.com>
2024-05-11 16:06:56 +0200

    Nodes 4 LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -12,8 +12,9 @@ class HTMLNode():
 
     def props_to_html(self):
         string = ""
-        for prop in self.props:
-            string += f' {prop}="{self.props[prop]}"'
+        if self.props:
+            for prop in self.props:
+                string += f' {prop}="{self.props[prop]}"'
         return string
     
 
@@ -23,6 +24,23 @@ class HTMLNode():
 
     def __eq__(self, value: object) -> bool:
         return self.tag==value.tag and self.value==value.value and self.children==value.children and self.props==value.props
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str, value: str, props: dict = None) -> None:
+        super().__init__(tag=tag, value=value, children=None, props=props)
+    
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
 
 if __name__ == "__main__":
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -11,6 +11,14 @@ class TestTextNode(unittest.TestCase):
         node = HTMLNode(tag="a", value="This is a text node", props={"href": "https://www.boot.dev"})
         node2 = HTMLNode(tag="a", value="This is a text node", props={"href": "https://www.boot.dev"})
         self.assertEqual(node, node2)
+
+        lnode = LeafNode("p", "This is a paragraph of text.")
+        rendered_text = "<p>This is a paragraph of text.</p>"
+        self.assertEqual(lnode.to_html(), rendered_text)
+
+        lnode = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        rendered_text = '<a href="https://www.google.com">Click me!</a>'
+        self.assertEqual(lnode.to_html(), rendered_text)
     
     def test_not_eq(self):
         node = HTMLNode(tag="a", value="This is a text node", props={"href": "https://www.boot.dev"})
@@ -20,6 +28,14 @@ class TestTextNode(unittest.TestCase):
         node = HTMLNode(tag="a", value="This is a text node", props={"href": "https://www.boot.dev"})
         node2 = HTMLNode(tag="a", value="This is a text node", props={"href": "https://some.different.link"})
         self.assertNotEqual(node, node2)
+
+        lnode = LeafNode("a", "Click here!", {"href": "https://www.google.com"})
+        rendered_text = '<a href="https://www.google.com">Click me!</a>'
+        self.assertNotEqual(lnode.to_html(), rendered_text)
+
+        lnode = LeafNode("a", "Click me!", {"href": "https://www.google.no"})
+        rendered_text = '<a href="https://www.google.com">Click me!</a>'
+        self.assertNotEqual(lnode.to_html(), rendered_text)
         
 
 if __name__ == "__main__":
