79bad48d8d3e6767e16e67fba2bf0ff36654a6ed
TrulsElg <trulseb@gmail.com>
2024-05-14 22:31:58 +0200

    3 Inline 3 Split images and links pt 1

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,14 @@
 import unittest
 
-from textnode import TextNode, split_nodes_delimiter, text_type_bold, text_type_code, text_type_italic, text_type_text
+from textnode import (TextNode, 
+                      split_nodes_delimiter,
+                      split_nodes_image, 
+                      text_type_bold, 
+                      text_type_code, 
+                      text_type_italic, 
+                      text_type_text,
+                      text_type_image,
+                      text_type_link,)
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -39,6 +47,22 @@ class TestTextNode(unittest.TestCase):
                         TextNode(" word", text_type_text),
                     ]
         self.assertEqual(new_nodes, exptected)
+    
+    def test_split_image_eq(self):
+        node = TextNode(
+            "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+                        TextNode("This is text with an ", text_type_text),
+                        TextNode("image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+                        TextNode(" and another ", text_type_text),
+                        TextNode(
+                            "second image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"
+                        ),
+                    ]
+        self.assertEqual(new_nodes, expected)
         
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,13 @@
+from extract_links import extract_markdown_links, extract_markdown_images
+
 text_type_text="text"
 text_type_bold="bold"
 text_type_italic="italic"
 text_type_code="code"
+text_type_image="image"
+text_type_link="link"
 
 def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: str):
-
     new_nodes = list()
     for node in old_nodes:
         if node.text_type != text_type_text:
@@ -23,7 +26,44 @@ def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: str):
             new_nodes.append(TextNode(text=section, text_type=node_type))
     
     return new_nodes
-        
+
+
+def split_nodes_image(old_nodes: list):
+    new_nodes = list()
+    for node in old_nodes:
+        images = extract_markdown_images(node.text)
+        if images:
+            non_image_sections = list()
+            non_image_sections.extend(node.text.split(f"![{images[0][0]}]({images[0][1]})", 1))
+            for image in images[1:]:
+                text_to_split = non_image_sections.pop()
+                non_image_sections.extend(text_to_split.split(f"![{image[0]}]({image[1]})", 1))
+
+            image_nodes = list()
+            for image in images:
+                image_nodes.append(TextNode(text=image[0], text_type=text_type_image, url=image[1]))
+            text_nodes = list()
+            for text in non_image_sections:
+                text_nodes.append(TextNode(text=text, text_type=text_type_text))
+            
+            print(image_nodes)
+            print(text_nodes)
+            l = list()
+            for i in range(max(len(image_nodes), len(text_nodes))):
+                if i+1 <= len(text_nodes):
+                    l.append(text_nodes[i])
+                if i+1 <= len(image_nodes):
+                    l.append(image_nodes[i])
+            
+            l = [n for n in l if n.text]
+            
+            new_nodes.extend(l)
+
+        else:
+            new_nodes.append(node)
+
+    return new_nodes
+
 
 
 class TextNode:
