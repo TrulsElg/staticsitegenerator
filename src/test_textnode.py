25584f85eb116565e106532cf4f958aa9330ce94
TrulsElg <trulseb@gmail.com>
2024-05-14 21:17:36 +0200

    3 Inline 1 Split Delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, split_nodes_delimiter, text_type_bold, text_type_code, text_type_italic, text_type_text
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -20,6 +20,25 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold", "https://www.boot.dev")
         node2 = TextNode("This is a text node", "bold")
         self.assertNotEqual(node, node2)
+    
+    def test_split_eq(self):
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        exptected = [
+                        TextNode("This is text with a ", text_type_text),
+                        TextNode("code block", text_type_code),
+                        TextNode(" word", text_type_text),
+                    ]
+        self.assertEqual(new_nodes, exptected)
+
+        node = TextNode("This is text with a *code block* word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "*", text_type_bold)
+        exptected = [
+                        TextNode("This is text with a ", text_type_text),
+                        TextNode("code block", text_type_bold),
+                        TextNode(" word", text_type_text),
+                    ]
+        self.assertEqual(new_nodes, exptected)
         
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,30 @@
+text_type_text="text"
+text_type_bold="bold"
+text_type_italic="italic"
+text_type_code="code"
+
+def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: str):
+
+    new_nodes = list()
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+
+        delim_splits = node.text.split(delimiter)
+        if len(delim_splits) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        
+        for index, section in enumerate(delim_splits):
+            if index % 2 == 0:
+                node_type = text_type_text
+            else:
+                node_type = text_type
+            new_nodes.append(TextNode(text=section, text_type=node_type))
+    
+    return new_nodes
+        
+
 
 class TextNode:
     def __init__(self, text, text_type, url=None) -> None:
